795dfb90d661aff319559bdb03c6b90a9b4257a1
fand1l <fand1l@outlook.com>
2025-11-09 18:58:37 +0200

    Add MinecraftServer admin interface

--- control_panel/admin.py
+++ control_panel/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import MinecraftServer
 
-# Register your models here.
+@admin.register(MinecraftServer)
+class MinecraftServerAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "status", "port", "software", "version")
+    list_filter = ("status", "software", "version")
+    search_fields = ("name", "owner__username")
+    readonly_fields = ("created_at", "pid", "server_id")
+
+    fieldsets = (
+        (None, {
+            "fields": ("name", "owner", "description")
+        }),
+        ("Server configuration", {
+            "fields": ("version", "software", "jar_path", "server_dir_location", "port")
+        }),
+        ("Resources & Status", {
+            "fields": ("allocated_memory", "allocated_cpu_threads", "status", "pid")
+        }),
+        ("Other", {
+            "fields": ("server_id", "created_at")
+        })
+    )
+
