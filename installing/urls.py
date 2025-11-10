37f3b8ebbbb11c9819ab16a8010ccfb88fe424fd
fand1l <fand1l@outlook.com>
2025-11-11 00:13:20 +0200

    Fix typo in URL routing for get_available_versions endpoint

--- installing/urls.py
+++ installing/urls.py
@@ -3,6 +3,6 @@ from .views import ServerCreateView, get_available_versions, get_download_info
 
 urlpatterns = [
     path('create/', view=ServerCreateView.as_view(), name="create_server"),
-    path('api/get-versions/', view=get_available_versions, name="get_available_view"),
+    path('api/get-versions/', view=get_available_versions, name="get_available_version"),
     path('api/get-download-info/', view=get_download_info, name="get_download_info")
 ]
